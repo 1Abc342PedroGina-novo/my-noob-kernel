2bcb0cbcefd77aaa40af490ed508ee5d57586497
1Abc342PedroGina-novo <rmagalhaesjuridico5@gmail.com>
2026-04-22 15:35:07 -0300

    Add kernel header file with function declarations

---
+++ inc/linux/kernel.h
@@ -0,0 +1,23 @@
+
+
+#ifndef _LINUX_KERNEL_H
+#define _LINUX_KERNEL_H
+
+void verify_area(void *addr, int count);
+void panic(const char *str);
+int printf(const char *fmt, ...);
+int printk(const char *fmt, ...);
+int tty_write(unsigned ch, char *buf, int count);
+void *malloc(unsigned int size);
+void free_s(void *obj, int size);
+
+#define free(x) free_s((x), 0)
+
+#define suser() (current->euid == 0)
+
+int init_srv(int srv_id, void *entry, int priority);
+void *pmap(unsigned long phys_addr, size_t size, int flags);
+int obj_mgr(int cmd, int obj_id, void *data);
+ssize_t ipc(int dest_id, const void *msg, size_t len, int flags);
+
+#endif
